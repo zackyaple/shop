eec62c13d631e3c36fa52c054a936cdd40fc2ad2
zackyaple <zack@yaple.net>
2023-02-13 21:20:06 -0500

    newfiles

---
+++ class.py
@@ -0,0 +1,40 @@
+class items:
+    def __init__(self, item):
+        self.item = item
+        clothing = clothing.printinfo
+        hat = hats.printinfo
+        plate = plates.printinfo
+    
+class clothing(items):
+#    def __init__(self, item, size):
+#        self.item = item
+#        self.size = size
+    def printinfo(self):
+        print(self.item)
+class hats():
+    pass
+    def __init__(self, item, color):
+        self.item = item
+        self.color = color
+    def printinfo(self):
+        print(self.item + self.color)
+class plates():
+    pass
+    def __init__(self, item):
+        self.item = item
+    def printinfo(self):
+        print(self.item)
+
+x = input("item?  ")
+order = items(x)
+if x == "shirt" or "hoodie":
+    order.clothing()
+elif x == "hats":
+    order.hat
+elif x == "plates":
+    order.plate
+else:
+    print("error")
+
+
+
---
+++ sqlite.py
@@ -0,0 +1,52 @@
+import pymysql.cursors
+
+#CREATE TABLE inventory(id INTERGER PRIMARY KEY, product_name TEXT, quantity INTEGER, UNIQUE(product_name), AUTO_INCREMENT=1)
+
+#connect to database file on machine
+connection = pymysql.connect(
+        host='local_host',
+        user='user',
+        password='passwd',
+        database='db',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+def newstock(product, starting_quantity):
+    with connection:
+        with connection.cursor() as cursor:
+            #create a new record
+            sql = "INSERT INTO 'inventory' ('product_name', 'quantity') VALUES (%s, %s)"
+            cursor.execute(sql, (product, starting_quantity))
+        #connections arent autocommited by defualt, so we commit changes next
+        connection.commit()
+
+        with connection.cursor() as cursor:
+            #read a single record contianing same variable name to confirm change
+            sql = "SELECT 'id', 'quantity' FROM 'inventory' WHERE 'product_name'=%S"
+            cursor.execute(sql, (product,))
+            result = cursor.fetchone()
+    connection.close()
+
+
+
+def update(product, new_quantity):
+    with connection:
+        with connection.cursor() as cursor:
+            #sets new quantity for product 
+            sql = "UPDATE 'inventory' SET 'product_name'=%s  WHERE 'quantity'=%s"
+            cursor.execute(sql, (product, new_quantity))
+       #connections arent autocommited by defualt, so we commit changes next
+        connection.commit()
+    connection.close()
+
+def remove(product):
+    with connection:
+        with connection.cursor() as cursor:
+            #removes product from table
+            sql = "DELETE FROM 'inventory' WHERE 'product'=%s"
+            cursor.execute(sql, (product))
+       #connections arent autocommited by defualt, so we commit changes next
+        connection.commit()
+    connection.close()
+
+
+
